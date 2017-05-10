9a94a93a5f0601fa5716a67a81c4db3358deec5e
张小七棒棒哒 <282129832@qq.com>
2017-05-10 18:02:12 +0800

    05101802

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+/*
+* 处理css脚本
+* author：seven
+* date:2017/5/9
+*
+*/
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');//CSS3自动补全插件
+const uglify = require('gulp-uglify');//压缩javascript文件
+const rename =require('gulp-rename');//改变管道中的文件名
+const jekyll =require('gulp-jekyll');
+
+
+
+/**
+ * 将_style文件夹的scss文件编译成css文件
+ */
+gulp.task('sass',() => {
+    return gulp.src('_style/gather.scss')
+        .pipe(sass({
+            // includePaths: ['scss'],
+            // onError: function(e) {
+            //     console.error(e);
+            // },
+            outputStyle: 'compressed'
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(rename('default.min.css'))
+        .pipe(gulp.dest('css'))
+});
+
+
+
+
+
+/**
+ * 监听_scss文件的变化
+ */
+gulp.task('watch',() => {
+    gulp.watch('_style/*.scss',[sass]);
+});
+
+
+
+
+
+gulp.task('default',['sass','watch']);
+
