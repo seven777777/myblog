53e588d4a3b80dccd829add86459afe3de060e79
seven <zhangmin145440@ehousechina.com>
2018-05-22 10:51:56 +0800

    footer修改

--- js/common.js
+++ js/common.js
@@ -2,39 +2,13 @@
  * @Author: seven.zhang 
  * @Date: 2018-05-18 14:22:14 
  * @Last Modified by: seven.zhang
- * @Last Modified time: 2018-05-18 17:35:52
+ * @Last Modified time: 2018-05-22 10:40:51
  */
 
 // 页面滚动事件
-// let toTop = $(window).scrollTop();
 let toTop=0,toTopAfter=0;
-// let scrollFunc = (e) => {  
-//     e = e || window.event;  
-//     if (e.wheelDelta) {  //第一步：先判断浏览器IE，谷歌滑轮事件               
-//         if (e.wheelDelta > 0) { //当滑轮向上滚动时  
-//             $('#nav-top').slideDown()
-//         }  
-//         if (e.wheelDelta < 0) { //当滑轮向下滚动时  
-//             console.log("滑轮向下滚动");  
-//         }  
-//     } else if (e.detail) {  //Firefox滑轮事件  
-//         if (e.detail> 0) { //当滑轮向上滚动时  
-//             $('#nav-top').slideDown()
-//         }  
-//         if (e.detail< 0) { //当滑轮向下滚动时  
-//             console.log("滑轮向下滚动");  
-//         }  
-//     }  
-// }
-// //给页面绑定滑轮滚动事件  
-// if (document.addEventListener) {//firefox  
-//     document.addEventListener('DOMMouseScroll', scrollFunc, false);  
-// }  
-// //滚动滑轮触发scrollFunc方法  //ie 谷歌  
-// window.onmousewheel = document.onmousewheel = scrollFunc;
-
 $(window).scroll(()=>{
-    let toTop = $(window).scrollTop();
+    toTop = $(window).scrollTop();
     if(toTop==0){
         $('#nav-top').removeClass('nav-top-show');
     }else{
@@ -54,4 +28,12 @@ $(window).scroll(()=>{
     setTimeout(() => {
         toTopAfter=toTop;
     }, 0);
+});
+
+// 尾部显示公众号二维码
+$('#gzh').mouseover(function(){
+    $(".gzh_ewm").fadeIn(300);
+});
+$('#gzh').mouseleave(function(){
+    $(".gzh_ewm").fadeOut(300);
 });
