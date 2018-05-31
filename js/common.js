3f30575f66173dffde715ca6b0bf45b5c2819b13
seven <zhangmin145440@ehousechina.com>
2018-05-31 11:05:32 +0800

    layout setting change

--- js/common.js
+++ js/common.js
@@ -2,28 +2,23 @@
  * @Author: seven.zhang 
  * @Date: 2018-05-18 14:22:14 
  * @Last Modified by: seven.zhang
- * @Last Modified time: 2018-05-30 16:55:37
+ * @Last Modified time: 2018-05-31 10:50:51
  */
 
 // 页面滚动事件
 let toTop=0,toTopAfter=0;
 $(window).scroll(()=>{
     toTop = $(window).scrollTop();
-    if(toTop==0){
-        $('#nav-top').removeClass('nav-top-show');
-    }else{
-        $('#nav-top').addClass('nav-top-show');
-        if(toTop>300){
-            if(toTop<=toTopAfter){//上滚
-                console.log('上滚')
-                // $('#nav-top').slideDown();
-                $('#nav-top').css("top","0");
-            }else{//下滚
-                console.log('下滚')
-                // $('#nav-top').slideUp();
-                $('#nav-top').removeClass('nav-top-show');
-                $('#nav-top').css("top","-100%");
-            }
+    if(toTop>300){
+        if(toTop<=toTopAfter){//上滚
+            console.log('上滚')
+            // $('#nav-top').slideDown();
+            $('#nav-top').css("top","0");
+        }else{//下滚
+            console.log('下滚')
+            // $('#nav-top').slideUp();
+            $('#nav-top').removeClass('nav-top-show');
+            $('#nav-top').css("top","-100%");
         }
     }
     setTimeout(() => {
