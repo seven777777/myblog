b3ded3f9fe9558ea423652fa3e6ce598cbd3835e
seven <zhangmin145440@ehousechina.com>
2018-05-31 17:13:20 +0800

    update

--- js/common.js
+++ js/common.js
@@ -2,29 +2,29 @@
  * @Author: seven.zhang 
  * @Date: 2018-05-18 14:22:14 
  * @Last Modified by: seven.zhang
- * @Last Modified time: 2018-05-31 10:50:51
+ * @Last Modified time: 2018-05-31 16:55:07
  */
 
 // 页面滚动事件
-let toTop=0,toTopAfter=0;
-$(window).scroll(()=>{
-    toTop = $(window).scrollTop();
-    if(toTop>300){
-        if(toTop<=toTopAfter){//上滚
-            console.log('上滚')
-            // $('#nav-top').slideDown();
-            $('#nav-top').css("top","0");
-        }else{//下滚
-            console.log('下滚')
-            // $('#nav-top').slideUp();
-            $('#nav-top').removeClass('nav-top-show');
-            $('#nav-top').css("top","-100%");
-        }
-    }
-    setTimeout(() => {
-        toTopAfter=toTop;
-    }, 0);
-});
+// let toTop=0,toTopAfter=0;
+// $(window).scroll(()=>{
+//     toTop = $(window).scrollTop();
+//     if(toTop>300){
+//         if(toTop<=toTopAfter){//上滚
+//             console.log('上滚')
+//             // $('#nav-top').slideDown();
+//             $('#nav-top').css("top","0");
+//         }else{//下滚
+//             console.log('下滚')
+//             // $('#nav-top').slideUp();
+//             $('#nav-top').removeClass('nav-top-show');
+//             $('#nav-top').css("top","-100%");
+//         }
+//     }
+//     setTimeout(() => {
+//         toTopAfter=toTop;
+//     }, 0);
+// });
 
 // 尾部显示公众号二维码
 $('#gzh').mouseover(function(){
