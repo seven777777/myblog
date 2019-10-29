be02788e58bb7212d8e0eec6312cbb92efae2d65
seven <zhangmin145440@ehousechina.com>
2019-10-29 17:37:42 +0800

    add

---
+++ js/static.config.js
@@ -0,0 +1,12 @@
+const categoryConfig = [
+    {
+        name:'work',
+        title:'工作',
+        desc:'技术相关的文章'
+    },
+    {
+        name:'life',
+        title:'生活',
+        desc:'生活随笔'
+    },
+]
